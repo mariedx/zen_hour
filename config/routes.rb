12705e56ff24889d0b922d2b29d7d3ebc5cc8ed7
MarieDechaux <marie.dechaux@gmail.com>
2021-09-15 14:36:35 -1000

    test new static page heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/feedbacks'
   get 'static_pages/offers'
+  get 'static_pages/testing'
 
   resources :messages, only: %i[new create]
 

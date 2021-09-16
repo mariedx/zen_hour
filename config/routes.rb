c5c781c603b1fbfaec6c44a227693ac5de623529
MarieDechaux <marie.dechaux@gmail.com>
2021-09-15 14:40:31 -1000

    edit test into offer static page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'static_pages/about'
   get 'static_pages/feedbacks'
-  get 'static_pages/offers'
-  get 'static_pages/testing'
-
+  get 'static_pages/offer'
+  
   resources :messages, only: %i[new create]
 
 end

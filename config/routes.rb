a32b5a407d1c8388b05da67abdf9107b7abc75d9
MarieDechaux <marie.dechaux@gmail.com>
2021-09-15 14:00:16 -1000

    drop database offers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'static_pages/about'
   get 'static_pages/feedbacks'
+  get 'static_pages/offers'
 
-  resources :offers
   resources :messages, only: %i[new create]
 
 end
